be195b6ce21976e2a2428039002e4132a00859cb
Mila1994 <madam.melon@mail.ru>
2022-08-07 18:04:23 +0300

    Задание 16.7.1

---
+++ practice_16/cat.py
@@ -0,0 +1,9 @@
+class Cat:
+    def __init__(self, name, sex, age):
+        self.name = name
+        self.sex = sex
+        self.age = age
+
+    def print_cat(self):
+        print(f"Cat name: {self.name}, cat sex: {self.sex}, cat age: {self.age}")
+
---
+++ practice_16/testCat.py
@@ -0,0 +1,8 @@
+from cat import Cat
+
+if __name__ == '__main__':
+    cat_sam = Cat("Сэм", "мальчик", 2)
+    cat_dosya = Cat("Дося", "девочка", 1.5)
+    cat_sam.print_cat()
+    cat_dosya.print_cat()
+
